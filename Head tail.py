6fc479ec75e35564127832939ed4a93a9d03f768
karim090606 <karimsab2006mulla@gmail.com>
2025-06-30 15:21:36 +0530

    Create Head tail.py

---
+++ Head tail.py
@@ -0,0 +1,17 @@
+print('Name:karim s mulla\nUSN: 1AY24AI052 \nSection: M\n ')
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input().lower()
+toss = random.randint(0, 1)  
+toss_str = 'heads' if toss == 1 else 'tails'
+if guess == toss_str:
+    print('You got it!')
+else:
+    print('Nope! Guess again:')
+    guess = input().lower()
+    if guess == toss_str:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
